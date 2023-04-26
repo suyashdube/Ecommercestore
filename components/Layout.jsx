f2fce2a004328170ffea8a92a2f169b5d2d89156
Suyash Dubey <suyash20251@iiitd.ac.in>
2023-04-27 02:16:33 +0530

    first commit

---
+++ components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>BEAT SOUND - Your Sound Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
